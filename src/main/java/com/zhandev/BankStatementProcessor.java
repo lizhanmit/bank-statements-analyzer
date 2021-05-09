1e45e7b3593d549cb879a0231fff9392fc777fe1
lizhanmit <lizhan_mit@outlook.com>
2021-05-09 16:28:28 +0800

    Refactored methods in BankStatementProcessor to apply open/close principle and lambda expression.

--- src/main/java/com/zhandev/BankStatementProcessor.java
+++ src/main/java/com/zhandev/BankStatementProcessor.java
@@ -1,6 +1,7 @@
 package com.zhandev;
 
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementProcessor {
@@ -11,40 +12,54 @@ public class BankStatementProcessor {
 		this.bankTransactions = bankTransactions;
 	}
 
-
-
-	public double calculateTotalAmount() {
-		double total = 0d;
+	public double summarizeTransactions(final BankTransactionSummarizer bankTransactionSummarizer) {
+		double result = 0d;
 		
 		for (final BankTransaction bankTransaction : bankTransactions) {
-			total += bankTransaction.getAmount();
+			result = bankTransactionSummarizer.summarize(result, bankTransaction);
 		}
 		
-		return total;
+		return result;
 	}
+
+	// use the below one that invokes the above method with open/close principle and lambda expression
+//	public double calculateTotalInMonth(final Month month) {
+//		double total = 0d;
+//		
+//		for (final BankTransaction bankTransaction : bankTransactions) {
+//			if (bankTransaction.getDate().getMonth() == month) {
+//				total += bankTransaction.getAmount();
+//			}
+//		}
+//		
+//		return total;
+//	}
 	
 	public double calculateTotalInMonth(final Month month) {
-		double total = 0d;
-		
-		for (final BankTransaction bankTransaction : bankTransactions) {
-			if (bankTransaction.getDate().getMonth() == month) {
-				total += bankTransaction.getAmount();
-			}
-		}
-		
-		return total;
+		return summarizeTransactions((acc, bankTransaction) -> 
+		bankTransaction.getDate().getMonth() == month ? acc + bankTransaction.getAmount() : acc);
 	}
 	
-	public double calculateTotalForCategory(final String category) {
-		double total = 0d;
+	
+	public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+		final List<BankTransaction> result = new ArrayList<>();
 		
 		for (final BankTransaction bankTransaction : bankTransactions) {
-			if (bankTransaction.getDescription().equals(category)) {
-				total += bankTransaction.getAmount();
+			if (bankTransactionFilter.test(bankTransaction)) {
+				result.add(bankTransaction);
 			}
 		}
 		
-		return total;
+		return result;
 	}
+	
+	// if this is a  very common operation, 
+	// it makes sense to extract it into an explicit API to make it easier for users to understand and use
+	public List<BankTransaction> findTransactionsGreaterThanEqual(final double amount) {
+		return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
+	}
+	
+
+	
 	
 }
