79a3ff3429697f55159a46d03df9a7a31bb07890
lizhanmit <lizhan_mit@outlook.com>
2021-06-06 20:51:14 +0800

    Added throwing CSVSyntaxException.

--- src/main/java/com/zhandev/BankStatementCSVParser.java
+++ src/main/java/com/zhandev/BankStatementCSVParser.java
@@ -7,10 +7,15 @@ import java.util.List;
 public class BankStatementCSVParser implements BankStatementParser {
 
 	private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyy");
+	private static final int EXPECTED_ATTRIBUTES_LENGTH = 3;
 	
 	@Override
 	public BankTransaction parseFrom(final String line) {
 		final String[] columns = line.split(",");
+		
+		if (columns.length < EXPECTED_ATTRIBUTES_LENGTH) {
+			throw new CSVSyntaxException();
+		}
 		
 		final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
 		final double amount = Double.parseDouble(columns[1]);
